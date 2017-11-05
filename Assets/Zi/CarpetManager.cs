2b7b31cdfee92f5284675f2a5d5a4992b62d470c
Zi Woythaler <woythale@usc.edu>
2017-11-04 21:30:20 -0700

    Sin wave almost working.......... `:)

--- Assets/Zi/CarpetManager.cs
+++ Assets/Zi/CarpetManager.cs
@@ -5,47 +5,49 @@ using UnityEngine;
 [RequireComponent(typeof(Transform))]
 public class CarpetManager : MonoBehaviour {
 
-	public int _initialSize;
-	public float _initialParticleScale;
-	public float _spacing;
+	public static float _spacing;
 	public Transform prefab;
 	public Transform transformTrigger;
 
+	[Header("Initializing")]
+	public int _initialSize;
+	public float _initialParticleScale;
+
 	[Header("Testing")]
 	public float peakHeight;
-	public float dampening;
+	public static float dampening;
 	public float multiplier = 0.01f;
 	public float speed = 10.0f;
 	public float phase = 1.0f;
 
 	private Transform [,] m_spheres;
 	private int _size;
-	private float _width;
+	private static float _width;
 
 	// Use this for initialization
 	void Start () {
+		//prefab = Resources.Load("Resources/BassCarpet/carpetSphere") as Transform;
+		//transformTrigger = Resources.Load("Resources/BassCarpet/transformTrigger") as Transform;
+		_spacing = 5;
 		_size = _initialSize;
 		m_spheres = new Transform[_size,_size];
-		_width = _size * _spacing;
+		_width =  ((float)_size) * _spacing;
 
 		Instantiate (transformTrigger,transform);
 
-		float xpos = 0, zpos = 0;
 		for (int x = 0; x < _size; x++)
 		{
 			for (int z = 0; z < _size; z++)
 			{
-				xpos = x; zpos = z;
-				m_spheres [x, z] = Instantiate (prefab);
+				m_spheres [x, z] = Instantiate (prefab) as Transform;
 				Transform t = m_spheres [x, z];
 				t.localScale = new Vector3 (1, 1, 1) * _initialParticleScale;
-				t.localPosition = new Vector3 (xpos - _width / 2.0f + xpos * _spacing, 0, zpos - _width / 2.0f + zpos * _spacing);
+				t.GetComponent <CarpetSphere>().SetGridLocation(new Vector2Int(x,z));
+				t.localPosition = GridToWorld ((float)x, (float)z);
 				t.SetParent (transform);
 			}
 		}
 	}
-
-	bool onn = true;
 
 	// Update is called once per frame
 	void Update () {
@@ -58,10 +60,30 @@ public class CarpetManager : MonoBehaviour {
 			{
 				Transform t = m_spheres [x, z];
 				Vector3 pos = t.position;
-				t.position = new Vector3(pos.x, pos.y + dampening * Mathf.Cos (time * Mathf.PI * 1.0f) * 1.0f, pos.z);
-//					(pos.y == 0 ? 1 : pos.y) * Mathf.Sin ((Time.time * speed) + (phase) * multiplier)
+				//t.position = new Vector3(pos.x, pos.y + (1- Mathf.Pow( Mathf.Min(time*0.1f, 1), 2))*Mathf.Cos (time * Mathf.PI * 1.0f)*1.0f, pos.z);
 				//m_spheres [x, z].localPosition = new Vector3 (xpos - _width / 2.0f + xpos * _spacing, 0, zpos - _width / 2.0f + zpos * _spacing);
 			}
 		}
+	}
+
+	void HandleEvent(float frequency, float amplitude)
+	{
+		int x = Mathf.RoundToInt(_width/2.0f), z = Mathf.RoundToInt(_width/2.0f);
+		Transform sphere = Instantiate (transformTrigger, GridToWorld (x, z), Quaternion.identity);
+		//generate the relevant information that the sphere needs
+		//figure out where we want the center to be
+		sphere.GetComponent<ExpandingSphere> ().Initialize(_size, new Vector2Int (x, z));
+	}
+
+	public static Vector3 GridToWorld(float x, float z)
+	{
+		return new Vector3 (x - _width / 2.0f + x * _spacing, 0, z - _width / 2.0f + z * _spacing);
+	}
+
+	public static Vector2Int WorldToGrid(Vector3 p)
+	{
+		float x = p.x, z = p.z;
+		return new Vector2Int (Mathf.RoundToInt((x + _width / 2.0f) / (1 + _spacing)),
+			Mathf.RoundToInt((z + _width / 2.0f) / (1 + _spacing)));
 	}
 }
