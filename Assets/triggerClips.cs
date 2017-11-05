a72e62f7bd2dfbd9c227e97d847bad7d69aae957
sarayd <saradubusiness@gmail.com>
2017-11-05 11:37:39 -0800

    pun!

--- Assets/triggerClips.cs
+++ Assets/triggerClips.cs
@@ -5,8 +5,13 @@ using UnityEngine.Events;
 using MidiJack;
 
 
+public class triggerClips : Photon.PunBehaviour {
 
-public class triggerClips : MonoBehaviour {
+	Vector3 leftTop; 
+	float width; 
+	float height;
+	public GameObject carp;
+
 
 	[System.Serializable]
 	public class MyIntEvent : UnityEvent<int>
@@ -16,6 +21,34 @@ public class triggerClips : MonoBehaviour {
 	// private bool colorToggle;
 	public int rootNote = 0;
 	// public GameObject bassCarpet;
+
+	void Awake()
+	{
+		PhotonNetwork.autoJoinLobby = false;
+		//might not be what we want 
+		PhotonNetwork.automaticallySyncScene = true;
+		Connect();
+	}
+
+	public void Connect()
+	{
+		if (PhotonNetwork.connected) {
+			PhotonNetwork.JoinRandomRoom ();
+		} else {
+			PhotonNetwork.ConnectUsingSettings ("1.0");
+		}
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log ("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+	{
+		PhotonNetwork.CreateRoom (null, new RoomOptions (){ MaxPlayers =4}, null);
+	}
 
 	void Start () {
 
@@ -30,12 +63,15 @@ public class triggerClips : MonoBehaviour {
 		void NoteOn(MidiChannel channel, int note, float velocity)
 		{
 
+
+
 		if (velocity > 0) {
-
 			for (int clipIndex = 0; clipIndex < 4; clipIndex++){
-
+				
 				if (note == clipIndex) {
-					triggerClip ((int) channel, clipIndex);
+					//PhotonView photonView = PhotonView.Get(this);
+					this.photonView.RPC("triggerClip", PhotonTargets.All, (int) channel, clipIndex);
+//					triggerClip ((int) channel, clipIndex);
 				}
 			}
 
@@ -43,7 +79,19 @@ public class triggerClips : MonoBehaviour {
 
 		}
 
+	[PunRPC]
 		void triggerClip(int group, int clipIndex){
+		int index = 4 * group + clipIndex;
+
+		//
+			float frequency  = 0.25f + index* (4-0.25f)/16;
+		Color color = Color.Lerp( Color.white, Color.blue ,  0.1f+index*0.9f/16);
+//		Vector3 coord =  Vector3 (width * group, 0, height * clipIndex);
+
+
+
+
+
 
 		Debug.Log ("Clip "+clipIndex+" in Group "+group+" has been triggered");
 
